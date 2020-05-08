7d13df3b4aafa22c7e62e14cd4d8ba095435c5cf
shaowen <jiayou123>
2020-05-08 14:54:46 +0800

    批量修改状态

--- src/views/monitor/eureka/index.vue
+++ src/views/monitor/eureka/index.vue
@@ -1,3 +1,3 @@
 <template>
-    <iframe src='http://localhost:8848/nacos/#/serviceManagement?dataId=&group=&appName=&namespace=' id='show' width='100%' height="780px" frameborder=0 name='showHere' ></iframe>
+    <iframe src='http://60.205.95.192:8848/nacos/#/serviceManagement?dataId=&group=&appName=&namespace=' id='show' width='100%' height="780px" frameborder=0 name='showHere' ></iframe>
 </template>
--- src/views/monitor/service/index.vue
+++ src/views/monitor/service/index.vue
@@ -1,3 +1,3 @@
 <template>
-    <iframe src='http://localhost:8764' id='show' width='100%' height="780px" frameborder=0 name='showHere' ></iframe>
+    <iframe src='http://60.205.95.192:8764' id='show' width='100%' height="780px" frameborder=0 name='showHere' ></iframe>
 </template>
