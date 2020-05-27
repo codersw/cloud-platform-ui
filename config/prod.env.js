0ed96e9af865cbf620f063e7f89a43444c513c1d
shaowen <jiayou123>
2020-05-27 13:34:44 +0800

    去掉多余的引用

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
     NODE_ENV: '"production"',
-    BASE_API: '"http://nw.sy-metro.com:9680"',
-    APP_ORIGIN: '"https://wallstreetcn.com"'
+    BASE_API: '"http://nw.sy-metro.com:9060"',
+    APP_ORIGIN: '"http://nw.sy-metro.com:9060"'
 };
