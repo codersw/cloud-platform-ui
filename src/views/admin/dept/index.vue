8fd13a2ca35dde1e93b10c63dab942b54ca8d2ae
shaowen <jiayou123>
2020-05-29 11:18:23 +0800

    去掉多余的引用

--- src/views/admin/dept/index.vue
+++ src/views/admin/dept/index.vue
@@ -62,7 +62,7 @@
           <el-select v-model="form.pid" filterable placeholder="请选择父级部门">
             <el-option
               v-for="item in deptTree"
-              :key="item.pid"
+              :key="item.id"
               :label="item.name"
               :value="item.id">
             </el-option>
